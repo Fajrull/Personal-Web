bdda4bff3094d2d0af8c32a6f3f05f3e6ea1c04b
Fajrull <fajrulm05@gmail.com>
2023-10-01 13:49:46 +0700

    upload file

---
+++ assets/js/script.js
@@ -0,0 +1,44 @@
+// mengirim pesan di google sheet
+
+const scriptURL =
+  "https://script.google.com/macros/s/AKfycbyC-N7pbEi3tTA-dEbwTK1xF2gMlXcf5uHWAj9XaKtpbfV3LH8IYAs9NnCcVOSeN-tu/exec";
+const form = document.forms["portofolio-contact-form"];
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  fetch(scriptURL, { method: "POST", body: new FormData(form) })
+    .then((response) => console.log("Success!", response))
+    .catch((error) => console.error("Error!", error.message));
+});
+
+// Fungsi memanggil data ketika sukses
+function handleGetFormData() {
+  return {
+    name: document.getElementById("name").value,
+    email: document.getElementById("email").value,
+    subject: document.getElementById("subject").value,
+  };
+}
+
+function validateFormData({ name, email, subject } = {}) {
+  return name && email && subject;
+}
+
+function submit() {
+  const formData = handleGetFormData();
+
+  if (validateFormData(formData)) {
+    swal({
+      title: "Good job!",
+      text: "Data berhasil dikirim!",
+      icon: "success",
+      button: "Aww yiss!",
+    });
+    // Code to handle successful form submission
+  }
+}
+
+document.querySelector("form").addEventListener("submit", (event) => {
+  event.preventDefault();
+  submit();
+});
